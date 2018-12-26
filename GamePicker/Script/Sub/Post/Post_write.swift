f5dcae2287b769d4cb4f16e443f6b78bf779dcd0
JHM <als091212@naver.com>
2018-12-27 00:03:24 +0900

    test

---
+++ GamePicker/Script/Sub/Post/Post_write.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class Post_write: UIViewController {
+    var game_id : Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(game_id)
+       
+    }
+
+
+}
